fdf7cd930b42a263c26b4c0b00474b7dd0e1576e
Ian <105128049+minijae011030@users.noreply.github.com>
2026-03-07 21:03:14 +0900

    chore: add request validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Request -> Pipe (Validation) -> Controller -> Service
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 없는 값은 자동 제거
+      forbidNonWhitelisted: true, // DTO에 없는 값이 있으면 에러 발생
+      transform: true, // 데이터 타입 자동 변환
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
